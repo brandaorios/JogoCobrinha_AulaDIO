3a277b128d180f7696eac7071b0cc17d41933714
Marcos Brandão Rios <brandaorios@gmail.com>
2021-06-27 00:04:26 -0300

    final de jogo

--- script.js
+++ script.js
@@ -40,10 +40,18 @@ function update (event){
 }
 
 function iniciarJogo(){
+
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    for(i = 1;  i < snake.length; i++){
+        if (snake[0].x == snake[i].x && snake[o].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
 
     criarBG();
     criarCobrinha();
@@ -72,4 +80,5 @@ function iniciarJogo(){
     snake.unshift(newHead); 
 
 }
+
 let jogo = setInterval(iniciarJogo, 100);
